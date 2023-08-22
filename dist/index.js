b43213a6e967da6c91410764c568d001212a82e5
Feras Khalil Musallam Alkhalailah <Fkmkh11@gmail.com>
2023-08-22 16:32:11 +0300

    some changes

--- dist/index.js
+++ dist/index.js
@@ -16,13 +16,16 @@ app.get('/books/:id', (req, res) => {
     res.send(Sample_data_js_1.data.filter(val => val.id == parseInt(id)));
 });
 app.post('/books/add', (req, res) => {
-    const { id, title, author, publicationYear } = req.body;
+    const id = req.body.id;
+    const title = req.body.title;
+    const author = req.body.author;
+    const publicationYear = req.body.publicationYear;
     if (id && title && author && publicationYear) {
         Sample_data_js_1.data.unshift(req.body);
-        res.send('data added successfully');
+        res.send('Data added successfully');
     }
     else {
-        res.status(400).send('how dare you not filling all feilds !!!!');
+        res.status(400).send('Please fill all fields.');
     }
 });
 app.put('/books/update/:id', (req, res) => {
@@ -50,19 +53,27 @@ app.delete('/books/delete/:id', (req, res) => {
 });
 app.get('/books/callbyname', (req, res) => {
     const title = req.query.title;
+    console.log("tested");
     if (!title || title.trim() === '') {
         return res.status(400).json({ error: 'Please provide a valid book title in the query parameter \'title\'.' });
     }
-    const matchedBooks = Sample_data_js_1.data.filter((book) => book.title.toLowerCase().includes(title.toLowerCase()));
-    res.json(matchedBooks);
+    const matchedBooks = Sample_data_js_1.data.filter((book) => {
+        console.log('Checking:', book.title); // Debug log to check which books are being compared
+        return book.title.includes(title);
+    });
+    console.log('Matched Books:', matchedBooks); // Debug log to see the matched books
+    res.send(matchedBooks);
 });
 app.get('/books/year', (req, res) => {
-    const year = req.query.year;
-    if (!year || isNaN(Number(year))) {
+    console.log('Received request for /books/year');
+    const year = Number(req.query.year);
+    if (isNaN(year)) {
+        console.log('Invalid year parameter:', req.query.year);
         return res.status(400).json({ error: 'Please provide a valid publishing year in the query parameter \'year\'.' });
     }
-    const publishingYear = parseInt(year);
-    const matchedBooks = Sample_data_js_1.data.filter((book) => book.publicationYear === publishingYear);
+    console.log('Valid year parameter:', year);
+    const matchedBooks = Sample_data_js_1.data.filter((book) => book.publicationYear === year);
+    console.log('Matched Books:', matchedBooks);
     res.send(matchedBooks);
 });
 const server = app.listen(port, () => {
--- index.ts
+++ index.ts
@@ -14,17 +14,20 @@ app.get('/books/:id', (req, res) => {
 })
 
 app.post('/books/add', (req, res) => {
-    const { id, title, author, publicationYear } = req.body;
+
+    const id = req.body.id;
+    const title = req.body.title;
+    const author = req.body.author;
+    const publicationYear = req.body.publicationYear;
+
     if (id && title && author && publicationYear) {
         data.unshift(req.body);
-        res.send('data added successfully')
+        res.send('Data added successfully');
+    } else {
+        res.status(400).send('Please fill all fields.');
     }
-    else {
-        res.status(400).send('how dare you not filling all feilds !!!!')
-    }
+});
 
-
-})
 app.put('/books/update/:id', (req, res) => {
     let existing;
     const { id, title, author, publicationYear } = req.body;
@@ -51,31 +54,43 @@ app.delete('/books/delete/:id', (req, res) => {
     res.send('the book is successfully deleted ')
 
 })
-app.get('/books/callbyname', (req, res) => {
-    const title = req.query.title as string;
+
   
-    if (!title || title.trim() === '') {
-      return res.status(400).json({ error: 'Please provide a valid book title in the query parameter \'title\'.' });
-    }
+  app.get('/books/callbyname', (req, res) => {
+      const title = req.query.title as string;
+      console.log("tested");
+      if (!title || title.trim() === '') {
+          return res.status(400).json({ error: 'Please provide a valid book title in the query parameter \'title\'.' });
+      }
   
-    const matchedBooks = data.filter((book) => book.title.toLowerCase().includes(title.toLowerCase()));
+      const matchedBooks = data.filter((book) => {
+          console.log('Checking:', book.title); // Debug log to check which books are being compared
+          return book.title.includes(title);
+      });
   
-    res.json(matchedBooks);
+      console.log('Matched Books:', matchedBooks); // Debug log to see the matched books
+      res.send(matchedBooks);
   });
+  
 
   app.get('/books/year', (req, res) => {
-    const year = req.query.year as string;
-  
-    if (!year || isNaN(Number(year))) {
-      return res.status(400).json({ error: 'Please provide a valid publishing year in the query parameter \'year\'.' });
+    console.log('Received request for /books/year');
+
+    const year = Number(req.query.year);
+
+    if (isNaN(year)) {
+        console.log('Invalid year parameter:', req.query.year);
+        return res.status(400).json({ error: 'Please provide a valid publishing year in the query parameter \'year\'.' });
     }
-  
-    const publishingYear = parseInt(year);
-    const matchedBooks = data.filter((book) => book.publicationYear === publishingYear);
-  
-    res.send(matchedBooks);
-  });
-  
+
+    console.log('Valid year parameter:', year);
+
+    const matchedBooks = data.filter((book) => book.publicationYear === year);
+
+    console.log('Matched Books:', matchedBooks);
+
+    res.send(matchedBooks)})
+
   
 
 
